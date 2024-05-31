5f81376399d72d6f520cf497aef206965f1374ce
lemon-lemolicious <lemon@lemolicious.com>
2024-05-31 00:00:37 -0400

    timeout

--- app.js
+++ app.js
@@ -140,9 +140,11 @@ class UI{
       clearCartBtn.addEventListener("click",()=>{
         document.getElementById("autoSetOrderDetails").value = JSON.stringify(Storage.getCart());
         document.getElementById("autoSetOrderTotal").value = cartTotal.innerText;
-        document.getElementById("gForm").reset();
-        document.getElementById("cartContent").innerHTML = "";
         this.clearCart();
+        setTimeout(function(){
+            document.getElementById("gForm").reset();
+            document.getElementById("cartContent").innerHTML = "";
+        }, 3000);
     });
      //cart functionality 
       cartContent.addEventListener ('click',event=>{
